938e6c83ca51232d64420d149eafc20617ae6b58
mpdairy <mpdairy@gmail.com>
2019-05-04 19:49:47 -0600

    ok

---
+++ src/Hinja/Types/BasicBlock.hs
@@ -0,0 +1,15 @@
+module Hinja.Types.BasicBlock where
+
+import Hinja.Prelude
+
+import Hinja.C.Pointers (BNBasicBlock)
+import Hinja.C.Types (InstructionIndex)
+
+data BasicBlock fun = BasicBlock
+  { _handle :: BNBasicBlock
+  , _func :: fun
+  , _start :: InstructionIndex fun
+  , _end :: InstructionIndex fun
+  } deriving (Eq, Ord, Show)
+
+$(makeFieldsNoPrefix ''BasicBlock)
