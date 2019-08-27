420c937f8faf5b21ba50528d08802dce7ba117a2
mpdairy <mpdairy@gmail.com>
2019-08-26 18:02:33 -0600

    checker sort of works

--- src/Haze/Solver.hs
+++ src/Haze/Solver.hs
@@ -6,3 +6,12 @@ import Data.SBV
 import qualified Data.SBV.String as S
 
 
+add5 :: SWord16 -> SWord16
+add5 n = n + 5
+
+bigtest :: Symbolic SBool
+bigtest = do
+  x <- exists "x"
+  y <- exists "y"
+  constrain $ add5 x .== (y :: SWord16)
+  return $ add5 x .== y
