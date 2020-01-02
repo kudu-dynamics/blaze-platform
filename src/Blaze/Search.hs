b4b8ab58c545c29f76f2a20d304c9dbcfcf3dfd9
mpdairy <mpdairy@gmail.com>
2020-01-01 20:40:16 -0700

    searchBetween helpers

--- src/Blaze/Search.hs
+++ src/Blaze/Search.hs
@@ -3,9 +3,11 @@ module Blaze.Search where
 import Blaze.Prelude hiding (succ, pred, toList)
 
 import Binja.Function (Function, MLILSSAFunction)
+import qualified Binja.Function as Func
 import Binja.Core (InstructionIndex, BNBinaryView)
 import qualified Blaze.Path as Path
-import Blaze.Types.Path (Path, Node)
+import Blaze.Types.Path (Path, Node, AbstractCallNode)
+import Blaze.Types.Graph (Graph)
 import qualified Blaze.Types.Function as BF
 import Blaze.Types.Function (CallSite)
 
@@ -27,15 +29,39 @@ nodeContainsInstruction ix x = case x of
     checkCallSite n = callSiteContainsInstruction ix $ n ^. Path.callSite
 
 -- | returns first node that contains the instruction
-pathContainsInstruction :: Path p => InstructionIndex F -> p -> Maybe Node
-pathContainsInstruction ix = headMay . filter (nodeContainsInstruction ix) . Path.toList
+firstNodeContainingInstruction :: Path p => InstructionIndex F -> p -> Maybe Node
+firstNodeContainingInstruction ix = headMay . filter (nodeContainsInstruction ix) . Path.toList
 
 
-searchBetween_ :: Graph () Function g
+-- | returns last node that contains the instruction
+-- todo: make a toRevList function that is just as fast as toList instead of reverse
+--      (for Path represented with a Graph)
+lastNodeContainingInstruction :: Path p => InstructionIndex F -> p -> Maybe Node
+lastNodeContainingInstruction ix = headMay . filter (nodeContainsInstruction ix) . reverse . Path.toList
+
+callSiteCallsFunction :: Function -> CallSite -> Bool
+callSiteCallsFunction fn c = case c ^. BF.callDest of
+  (BF.DestAddr addr) -> fn ^. Func.start == addr
+  (BF.DestFunc fn') -> fn == fn'
+  (BF.DestExpr _) -> False -- maybe should check the expr?
+  (BF.DestColl s) -> any g s
+    where
+      g (BF.DestCollAddr addr) = fn ^. Func.start == addr
+      g (BF.DestCollExpr _) = False -- should we check expr?
+
+getAbstractCallNodesToFunction :: Path p => Function -> p -> [AbstractCallNode]
+getAbstractCallNodesToFunction fn = mapMaybe f . Path.toList
+  where
+    f (Path.AbstractCall n) = bool Nothing (Just n) $ callSiteCallsFunction fn $ n ^. Path.callSite
+    f _ = Nothing
+
+-- | this is using the inefficent method of searching though all the nodes
+-- of every path in each function along the call path.
+searchBetween_ :: (Graph () Function g, Path p)
                => BNBinaryView
                -> g
-               -> Map Function [Path]
+               -> Map Function [p]
                -> Function -> InstructionIndex MLILSSAFunction
                -> Function -> InstructionIndex MLILSSAFunction
-               -> IO [Path]
+               -> IO [p] -- maybe doesn't need IO?
 searchBetween_ bv cfg fpaths fn1 ix1 fn2 ix2 = undefined
--- src/Blaze/Types/Path.hs
+++ src/Blaze/Types/Path.hs
@@ -24,59 +24,16 @@ class Path p where
   firstNode :: p -> Maybe Node
   lastNode :: p -> Maybe Node
   expandNode :: AbstractPathNode -> p -> p -> p
+
+  -- first p is path to replace AbstractPathNode
+  expandAbstractCall :: AbstractCallNode -> p -> p -> p
+  
   contains :: Node -> p -> Bool
 
 newtype PathGraph g = PathGraph g
 
 deriving instance (Graph () Node g) => Graph () Node (PathGraph g)
 
-instance (Graph () Node g) => Path (PathGraph g) where
-  toList g = case firstNode g of
-    Nothing -> []
-    Just x -> x:(getRest $ succ x g)
-      where
-        getRest Nothing = []
-        getRest (Just y) = y : getRest (succ y g)
-
-  fromList [] = G.empty
-  fromList [a] = G.fromNode a
-  fromList (x:xs) = G.fromEdges . fmap ((),) $ zip (x:xs) xs
-
-  succ node g = case Set.toList $ G.succs node g of
-    [] -> Nothing
-    [x] -> Just x
-    _ -> P.error "Path node has multiple successors. Bad!"
-
-  pred node g = case Set.toList $ G.preds node g of
-    [] -> Nothing
-    [x] -> Just x
-    _ -> P.error "Path node has multiple predecessors. Bad!"
-
-  firstNode g = case Set.toList $ G.sources g of
-    [] -> Nothing
-    [x] -> Just x
-    _ -> P.error "Path has multiple source nodes. Bad!"
-
-  lastNode g = case Set.toList $ G.sinks g of
-    [] -> Nothing
-    [x] -> Just x
-    _ -> P.error "Path has multiple sink nodes. Bad!"
-
-  expandNode apn gpart g = maybe g identity $ do
-    firstN <- firstNode gpart
-    lastN <- lastNode gpart
-    npred <- pred n g
-    nsucc <- succ n g
-    let g' = G.removeEdges [(npred, n), (n, nsucc)] g
-        g'' = flip G.addEdges g'
-              . fmap ((),) $ [(npred, firstN), (lastN, nsucc)] <> gpartEdges
-    return g''
-    where
-      gpartList = toList gpart
-      gpartEdges = zip gpartList (drop 1 gpartList)
-      n = AbstractPath apn
-
-  contains = G.hasNode
 
 -- instance Graph edge node g => Path (PathGraph g) where
 --   fromList :: (Graph () Node g) => [Node] -> PathGraph g
@@ -141,6 +98,77 @@ $(makeFieldsNoPrefix ''CallNode)
 $(makeFieldsNoPrefix ''RetNode)
 $(makeFieldsNoPrefix ''AbstractPathNode)
 $(makeFieldsNoPrefix ''AbstractCallNode)
+
+
+instance (Graph () Node g) => Path (PathGraph g) where
+  toList g = case firstNode g of
+    Nothing -> []
+    Just x -> x:(getRest $ succ x g)
+      where
+        getRest Nothing = []
+        getRest (Just y) = y : getRest (succ y g)
+
+  fromList [] = G.empty
+  fromList [a] = G.fromNode a
+  fromList (x:xs) = G.fromEdges . fmap ((),) $ zip (x:xs) xs
+
+  succ node g = case Set.toList $ G.succs node g of
+    [] -> Nothing
+    [x] -> Just x
+    _ -> P.error "Path node has multiple successors. Bad!"
+
+  pred node g = case Set.toList $ G.preds node g of
+    [] -> Nothing
+    [x] -> Just x
+    _ -> P.error "Path node has multiple predecessors. Bad!"
+
+  firstNode g = case Set.toList $ G.sources g of
+    [] -> Nothing
+    [x] -> Just x
+    _ -> P.error "Path has multiple source nodes. Bad!"
+
+  lastNode g = case Set.toList $ G.sinks g of
+    [] -> Nothing
+    [x] -> Just x
+    _ -> P.error "Path has multiple sink nodes. Bad!"
+
+  expandNode apn gpart g = maybe g identity $ do
+    firstN <- firstNode gpart
+    lastN <- lastNode gpart
+    npred <- pred n g
+    nsucc <- succ n g
+    let g' = G.removeEdges [(npred, n), (n, nsucc)] g
+        g'' = flip G.addEdges g'
+              . fmap ((),) $ [(npred, firstN), (lastN, nsucc)] <> gpartEdges
+    return g''
+    where
+      gpartList = toList gpart
+      gpartEdges = zip gpartList (drop 1 gpartList)
+      n = AbstractPath apn
+
+  expandAbstractCall acn ppart p = maybe p identity $ do
+    firstN <- firstNode ppart
+    lastN <- lastNode ppart
+    npred <- pred n p
+    nsucc <- succ n p
+    --- reusing the AbstractCallNode's uuid because we're not in IO
+    --  and the uuid is just there to make sure nodes of the same type
+    --  are distinguishable.
+    let cnode = Call $ CallNode (acn ^. func) (acn ^. callSite) (acn ^. uuid)
+        rnode = Ret $ RetNode (acn ^. func) (acn ^. callSite) (acn ^. uuid)
+    let p' = G.removeEdges [(npred, n), (n, nsucc)] p
+        p'' = flip G.addEdges p'
+              . fmap ((),) $ [ (npred, cnode)
+                             , (cnode, firstN)
+                             , (lastN, rnode)
+                             , (rnode, nsucc) ] <> ppartEdges
+    return p''
+    where
+      ppartList = toList ppart
+      ppartEdges = zip ppartList (drop 1 ppartList)
+      n = AbstractCall acn
+
+  contains = G.hasNode
 
 
 startFunction :: Path p => p -> Maybe Function
