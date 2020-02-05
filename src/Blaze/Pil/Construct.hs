32ee988ca760350e10942295bab8b83ba5afe877
mpdairy <mpdairy@gmail.com>
2020-02-05 15:35:08 -0700

    binOp in construct module

--- src/Blaze/Pil/Construct.hs
+++ src/Blaze/Pil/Construct.hs
@@ -2,29 +2,48 @@ module Blaze.Pil.Construct where
 
 import Blaze.Prelude hiding (const, sym)
 
-import qualified Blaze.Types.Pil as TPil
-
+import qualified Blaze.Types.Pil as Pil
+import Blaze.Types.Pil ( ExprOp
+                       , Expression(Expression)
+                       , OperationSize)
 import qualified Data.HashSet as HSet
 
-pilVar :: TPil.Symbol -> TPil.PilVar
-pilVar s = TPil.PilVar { _symbol   = s
+pilVar :: Pil.Symbol -> Pil.PilVar
+pilVar s = Pil.PilVar { _symbol   = s
                        , _func     = Nothing
                        , _ctxIndex = Nothing
                        , _mapsTo   = HSet.empty
                        }
 
-const :: Int64 -> TPil.OperationSize -> TPil.Expression
-const x size = TPil.Expression { _size = size 
-                               , _op = TPil.CONST (TPil.ConstOp x) }
+const :: Int64 -> Pil.OperationSize -> Pil.Expression
+const x size = Pil.Expression { _size = size 
+                               , _op = Pil.CONST (Pil.ConstOp x) }
 
-var :: TPil.Symbol -> TPil.OperationSize -> TPil.Expression
-var sym size = TPil.Expression { _size = size 
-                               , _op = TPil.VAR $ TPil.VarOp $ pilVar sym}
+var :: Pil.Symbol -> Pil.OperationSize -> Pil.Expression
+var sym size = Pil.Expression { _size = size 
+                               , _op = Pil.VAR $ Pil.VarOp $ pilVar sym}
 
-def :: TPil.Symbol -> TPil.Expression -> TPil.Stmt
-def sym expr = TPil.Def (TPil.DefOp (pilVar sym) expr)
+def :: Pil.Symbol -> Pil.Expression -> Pil.Stmt
+def sym expr = Pil.Def (Pil.DefOp (pilVar sym) expr)
 
-add :: TPil.Expression -> TPil.Expression -> TPil.OperationSize -> TPil.Expression
-add x y size = TPil.Expression { _size = size
-                               , _op = TPil.ADD (TPil.AddOp x y)}
+binOp :: (a -> ExprOp Expression)
+  -> (Expression -> Expression -> a)
+  -> OperationSize -> Expression -> Expression -> Expression
+binOp f g size x y = Expression { _size = size
+                                , _op = f (g x y)
+                                }
+
+add :: OperationSize -> Expression -> Expression -> Expression
+add = binOp Pil.ADD Pil.AddOp
+
+sub :: OperationSize -> Expression -> Expression -> Expression
+sub = binOp Pil.SUB Pil.SubOp
+
+cmpE :: OperationSize -> Expression -> Expression -> Expression
+cmpE = binOp Pil.CMP_E Pil.CmpEOp
+
+
+-- add :: Expression -> Expression -> OperationSize -> Expression
+-- add x y size = Expression { _size = size
+--                           , _op = Pil.ADD (Pil.AddOp x y)}
 
