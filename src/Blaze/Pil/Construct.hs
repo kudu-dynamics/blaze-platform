6181d8522dfc912bb3478e8efc6299d5e787d1f5
drone <drone@kududyn.com>
2020-02-08 11:23:48 -0700

    Added additional constructors. Merged some helpers from @mattp. Formatting experiment.

--- src/Blaze/Pil/Construct.hs
+++ src/Blaze/Pil/Construct.hs
@@ -1,30 +1,71 @@
 module Blaze.Pil.Construct where
 
-import Blaze.Prelude hiding (const, sym)
-
-import qualified Blaze.Types.Pil as TPil
-
+import Blaze.Prelude hiding (Symbol, const, sym)
+import qualified Blaze.Types.Pil as Pil
+import Blaze.Types.Pil
+  ( ExprOp,
+    Expression (Expression),
+    OperationSize,
+    PilVar (PilVar),
+    Stmt,
+    Symbol,
+  )
 import qualified Data.HashSet as HSet
 
-pilVar :: TPil.Symbol -> TPil.PilVar
-pilVar s = TPil.PilVar { _symbol   = s
-                       , _func     = Nothing
-                       , _ctxIndex = Nothing
-                       , _mapsTo   = HSet.empty
-                       }
+pilVar :: Symbol -> PilVar
+pilVar s =
+  PilVar
+    { _symbol = s,
+      _func = Nothing,
+      _ctxIndex = Nothing,
+      _mapsTo = HSet.empty
+    }
 
-const :: Int64 -> TPil.OperationSize -> TPil.Expression
-const x size = TPil.Expression { _size = size 
-                               , _op = TPil.CONST (TPil.ConstOp x) }
+mkExpr :: OperationSize -> ExprOp Expression -> Expression
+mkExpr size op =
+  Expression
+    { _size = size,
+      _op = op
+    }
 
-var :: TPil.Symbol -> TPil.OperationSize -> TPil.Expression
-var sym size = TPil.Expression { _size = size 
-                               , _op = TPil.VAR $ TPil.VarOp $ pilVar sym}
+binOp ::
+  (a -> ExprOp Expression) ->
+  (Expression -> Expression -> a) ->
+  Expression ->
+  Expression ->
+  OperationSize ->
+  Expression
+binOp f g x y size =
+  Expression
+    { _size = size,
+      _op = f (g x y)
+    }
 
-def :: TPil.Symbol -> TPil.Expression -> TPil.Stmt
-def sym expr = TPil.Def (TPil.DefOp (pilVar sym) expr)
+---- Expressions
+const :: Int64 -> OperationSize -> Expression
+const x size = mkExpr size (Pil.CONST (Pil.ConstOp x))
 
-add :: TPil.Expression -> TPil.Expression -> TPil.OperationSize -> TPil.Expression
-add x y size = TPil.Expression { _size = size
-                               , _op = TPil.ADD (TPil.AddOp x y)}
+var :: Symbol -> OperationSize -> Expression
+var sym size = mkExpr size (Pil.VAR $ Pil.VarOp $ pilVar sym)
 
+add :: Expression -> Expression -> OperationSize -> Expression
+add = binOp Pil.ADD Pil.AddOp
+
+sub :: Expression -> Expression -> OperationSize -> Expression
+sub = binOp Pil.SUB Pil.SubOp
+
+cmpE :: Expression -> Expression -> OperationSize -> Expression
+cmpE = binOp Pil.CMP_E Pil.CmpEOp
+
+loadSSA :: Expression -> Int64 -> OperationSize -> Expression
+loadSSA addr memVer size = mkExpr size (Pil.LOAD_SSA (Pil.LoadSSAOp addr memVer))
+
+---- Statements
+def :: Symbol -> Expression -> Stmt
+def sym val = Pil.Def (Pil.DefOp (pilVar sym) val)
+
+store :: Expression -> Expression -> Stmt
+store addr val = Pil.Store (Pil.StoreOp addr val)
+
+constraint :: Expression -> Stmt
+constraint e = Pil.Constraint (Pil.ConstraintOp e)
