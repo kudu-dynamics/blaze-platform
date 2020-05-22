e7865744ecb0c873ff7a8d455ce9a3c2cbbfb142
drone <drone@kududyn.com>
2020-05-22 09:43:52 -0600

    WIP: Mostly finished with CFG importing. Need to add import of edges.

--- src/Blaze/Types/Import/BinaryNinja.hs
+++ src/Blaze/Types/Import/BinaryNinja.hs
@@ -6,7 +6,7 @@ import qualified Binja.BasicBlock as BNBb
 import qualified Binja.Core as Binja
 import Binja.Core (BNBinaryView, BNSymbol)
 import qualified Binja.Function as BNFunc
-import qualified Binja.MLIL as MLIL
+import qualified Binja.MLIL as Mlil
 import qualified Binja.Reference as Ref
 import qualified Binja.View
 import Blaze.Function (CallInstruction, toCallInstruction)
@@ -22,18 +22,31 @@ import Blaze.Types.CallGraph
         _callSiteCaller,
         _callSiteDest
       ),
+    Function,
   )
 import Blaze.Types.Cfg
   ( CfEdge (CfEdge),
-    CfNode (BasicBlock, Call),
-    Cfg (Cfg),
-    ControlFlowGraph,
+    CfNode
+      ( BasicBlock,
+        Call,
+        _basicBlockFunction,
+        _basicBlockStart,
+        _callFunction,
+        _callTarget
+      ),
+    Cfg,
+    buildCfg,
   )
 import Control.Monad.Extra (mapMaybeM)
-import Control.Monad.Trans.Maybe (MaybeT (MaybeT, runMaybeT))
+import Control.Monad.Trans.Writer.Lazy (WriterT, runWriterT, tell)
 import Data.BinaryAnalysis (Address, Symbol (Symbol, _symbolName, _symbolRawName))
+import Data.DList (DList)
+import qualified Data.DList as DList
+import qualified Data.HashMap.Strict as HMap
+import qualified Data.List.NonEmpty as NEList
 import Data.Set as Set
 import qualified Data.Text as Text
+import Prelude (error)
 
 newtype BNImporter
   = BNImporter
@@ -74,20 +87,20 @@ getCallInstruction :: BNFunc.Function -> Ref.ReferenceSource -> IO (Maybe CallIn
 getCallInstruction caller ref = do
   llilFunc <- BNFunc.getLLILFunction caller
   llilIndex <- Binja.getLLILInstructionIndexAtAddress caller (ref ^. Ref.arch) (ref ^. Ref.addr)
-  mlilIndex <- MLIL.getMLILFromLLIL llilFunc llilIndex
+  mlilIndex <- Mlil.getMLILFromLLIL llilFunc llilIndex
   mlilFunc <- BNFunc.getMLILFunction caller
   mlilSSAFunc <- BNFunc.getMLILSSAFunction caller
-  mlilSSAIndex <- MLIL.getMLILSSSAFromMLIL mlilFunc mlilIndex
-  toCallInstruction <$> MLIL.instruction mlilSSAFunc mlilSSAIndex
+  mlilSSAIndex <- Mlil.getMLILSSSAFromMLIL mlilFunc mlilIndex
+  toCallInstruction <$> Mlil.instruction mlilSSAFunc mlilSSAIndex
 
-type MLILSSAOp = MLIL.Operation (MLIL.Expression BNFunc.MLILSSAFunction)
+type MLILSSAOp = Mlil.Operation (Mlil.Expression BNFunc.MLILSSAFunction)
 
 getCallDestAddr :: CallInstruction -> Maybe Address
 getCallDestAddr ci =
   case ci ^. Func.dest of
     Just dexpr ->
-      case (dexpr ^. MLIL.op :: MLILSSAOp) of
-        (MLIL.CONST_PTR cpop) -> Just $ fromIntegral $ cpop ^. MLIL.constant
+      case (dexpr ^. Mlil.op :: MLILSSAOp) of
+        (Mlil.CONST_PTR cpop) -> Just $ fromIntegral $ cpop ^. Mlil.constant
         _ -> Nothing
     _ -> Nothing
 
@@ -135,43 +148,161 @@ instance CallGraphImporter BNImporter where
         let callSiteArgs = catMaybes $ uncurry getMaybeCallerAndInstr <$> zip callers mCallInstrs
         mapMaybeM (uncurry $ createCallSite bv) callSiteArgs
 
-
 ---- CFG
+type MlilSsaFunc = BNFunc.MLILSSAFunction
 
-buildCfg :: [BNBb.BasicBlock BNFunc.MLILSSAFunction] ->
-            [BNBb.BlockEdge BNFunc.MLILSSAFunction] ->
-            Cfg BNImporter
-buildCfg bnNodes bnEdges = 
-  _
-    where
-      cfNodes :: [CfNode]
-      cfNodes = _
-      cfEdges :: [CfEdge]
-      cfEdges = _
+type MlilSsaBlock = BNBb.BasicBlock MlilSsaFunc
 
-instance CfgImporter BNImporter where
+type MlilSsaBlockEdge = BNBb.BlockEdge MlilSsaFunc
+
+type MlilSsaInstruction = Mlil.Instruction MlilSsaFunc
+
+type MlilSsaInstructionIndex = Binja.InstructionIndex MlilSsaFunc
+
+type NonCallInstruction = MlilSsaInstruction
+
+data InstrGroup
+  = SingleCall {_callInstr :: CallInstruction}
+  | ManyNonCalls {_nonCallInstrs :: NonEmpty NonCallInstruction}
+
+data MlilSsaInstr
+  = MlilSsaCall CallInstruction
+  | MlilSsaNonCall NonCallInstruction
+
+toMlilSsaInstr :: MlilSsaInstruction -> MlilSsaInstr
+toMlilSsaInstr instr =
+  maybe (MlilSsaNonCall instr) MlilSsaCall (toCallInstruction instr)
+
+data CodeReference
+  = CodeReference
+      { _function :: Function,
+        _startIndex :: MlilSsaInstructionIndex,
+        _endIndex :: MlilSsaInstructionIndex
+      }
+
+type NodeMap = HashMap CfNode CodeReference
+
+type NodeMapEntry = (CfNode, CodeReference)
+
+type NodeConverter a = WriterT (DList NodeMapEntry) IO a
+
+runNodeConverter :: NodeConverter a -> IO (a, DList NodeMapEntry)
+runNodeConverter = runWriterT
+
+tellEntry :: NodeMapEntry -> NodeConverter ()
+tellEntry = tell . DList.singleton
+
+-- | Assumes instructions are consecutive
+nodeFromInstrs :: Function -> NonEmpty NonCallInstruction -> NodeConverter CfNode
+nodeFromInstrs func instrs = do
+  let node =
+        BasicBlock
+          { _basicBlockFunction = func,
+            _basicBlockStart = (^. Mlil.address) . NEList.head $ instrs
+          }
+  tellEntry
+    ( node,
+      CodeReference
+        { _function = func,
+          _startIndex = (^. Mlil.index) . NEList.head $ instrs,
+          _endIndex = (^. Mlil.index) . NEList.last $ instrs
+        }
+    )
+  return node
+
+nodeFromCallInstr :: Function -> CallInstruction -> NodeConverter CfNode
+nodeFromCallInstr func callInstr = do
+  let node =
+        Call
+          { _callFunction = func,
+            _callTarget = callInstr ^. Func.address
+          }
+  tellEntry
+    ( node,
+      CodeReference
+        { _function = func,
+          _startIndex = callInstr ^. Func.index,
+          _endIndex = callInstr ^. Func.index
+        }
+    )
+  return node
+
+nodeFromGroup :: Function -> InstrGroup -> NodeConverter CfNode
+nodeFromGroup func = \case
+  (SingleCall x) -> nodeFromCallInstr func x
+  (ManyNonCalls xs) -> nodeFromInstrs func xs
+
+-- | Parse a list of MlilSsaInstr into groups
+groupInstrs :: [MlilSsaInstr] -> [InstrGroup]
+groupInstrs xs = mconcat (parseAndSplit <$> groupedInstrs)
+  where
+    groupedInstrs :: [NonEmpty MlilSsaInstr]
+    groupedInstrs =
+      NEList.groupWith
+        ( \case
+            (MlilSsaCall _) -> True
+            (MlilSsaNonCall _) -> False
+        )
+        xs
+    parseCall :: MlilSsaInstr -> Maybe CallInstruction
+    parseCall = \case
+      (MlilSsaCall x') -> Just x'
+      _ -> error "Unexpected non-call instruction when parsing call instructions."
+    parseNonCall :: MlilSsaInstr -> Maybe NonCallInstruction
+    parseNonCall = \case
+      (MlilSsaNonCall x') -> Just x'
+      _ -> error "Unexpected call instruction when parsing non-call instructions."
+    -- Need to split consecutive calls
+    parseAndSplit :: NonEmpty MlilSsaInstr -> [InstrGroup]
+    parseAndSplit g = case g of
+      -- In this case we know all instructions in the group will be calls
+      -- based on the previous grouping performed.
+      (MlilSsaCall _) :| _ -> mapMaybe (fmap SingleCall . parseCall) $ NEList.toList g
+      -- This case handles non-call instructions. We know there are no calls
+      -- in the group.
+      _ -> [ManyNonCalls . NEList.fromList . mapMaybe parseNonCall . NEList.toList $ g]
+
+nodesFromInstrs :: Function -> [MlilSsaInstruction] -> NodeConverter [CfNode]
+nodesFromInstrs func instrs = do
+  let instrGroups = groupInstrs $ toMlilSsaInstr <$> instrs
+  mapM (nodeFromGroup func) instrGroups
+
+convertNode :: Function -> MlilSsaBlock -> NodeConverter [CfNode]
+convertNode func bnBlock = do
+  instrs <- liftIO $ Mlil.fromBasicBlock bnBlock
+  nodesFromInstrs func instrs
+
+createEdgesForNodeGroup :: [CfNode] -> [CfEdge]
+createEdgesForNodeGroup ns =
+  maybe [] (uncurry CfEdge <$>) (maybeNodePairs =<< NEList.nonEmpty ns)
+  where
+    maybeNodePairs :: NonEmpty CfNode -> Maybe [(CfNode, CfNode)]
+    maybeNodePairs = \case
+      _ :| [] -> Nothing
+      nodes -> Just $ zip (NEList.toList nodes) (NEList.tail nodes)
+
+importCfg ::
+  Function ->
+  [MlilSsaBlock] ->
+  [MlilSsaBlockEdge] ->
+  IO (Cfg NodeMap)
+importCfg func bnNodes bnEdges = do
+  (cfNodeGroups, mapEntries) <- runNodeConverter $ mapM (convertNode func) bnNodes
+  let cfEdgesFromNodeGroups = concatMap createEdgesForNodeGroup cfNodeGroups
+  return $ buildCfg (concat cfNodeGroups) cfEdges (Just . HMap.fromList . DList.toList $ mapEntries)
+  where
+    cfEdges :: [CfEdge]
+    cfEdges = _
+
+instance CfgImporter BNImporter NodeMap where
   getCfg imp func = do
     let bv = imp ^. binaryView
     mBnFunc <- BNFunc.getFunctionStartingAt bv Nothing (func ^. CG.address)
     case mBnFunc of
-      Nothing -> 
+      Nothing ->
         return Nothing
       (Just bnFunc) -> do
         bnMlilFunc <- BNFunc.getMLILSSAFunction bnFunc
         bnMlilBbs <- BNBb.getBasicBlocks bnMlilFunc
         bnMlilBbEdges <- concatMapM BNBb.getOutgoingEdges bnMlilBbs
-        return $ Just (buildCfg bnMlilBbs bnMlilBbEdges)
-  -- getCfg imp func = do
-  --   let bv = imp ^. binaryView
-  --   runMaybeT $ do
-  --     bnFunc <- MaybeT $ BNFunc.getFunctionStartingAt bv Nothing (func ^. CG.address)
-  --     bnMlilFunc <- liftIO $ BNFunc.getMLILSSAFunction bnFunc
-  --     bnMlilBbs <- liftIO $ BNBb.getBasicBlocks bnMlilFunc
-  --     bnMlilBbEdges <- liftIO $ concatMapM BNBb.getOutgoingEdges bnMlilBbs
-  --     return $ buildCfg bnMlilBbs bnMlilBbEdges
-  --   where
-  --     buildCfg ::
-  --       [BNBb.BasicBlock BNFunc.MLILSSAFunction] ->
-  --       [BNBb.BlockEdge BNFunc.MLILSSAFunction] ->
-  --       Cfg BNImporter
-  --     buildCfg = undefined
+        Just <$> importCfg func bnMlilBbs bnMlilBbEdges
