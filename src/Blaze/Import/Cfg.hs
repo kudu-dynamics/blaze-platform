1dad0c6ce8b44b47e8401edea09f61b10da0d252
drone <drone@kududyn.com>
2020-05-25 16:38:09 -0600

    Removed outdated todo comment.

--- src/Blaze/Import/Cfg.hs
+++ src/Blaze/Import/Cfg.hs
@@ -4,6 +4,5 @@ import Blaze.Prelude
 import Blaze.Types.CallGraph (Function)
 import Blaze.Types.Cfg
 
--- TODO: (Cfg a) should reall be (Cfg ImporterSpecificMetadata)
 class CfgImporter a b | a -> b where
   getCfg :: a -> Function -> IO (Maybe (Cfg b))
