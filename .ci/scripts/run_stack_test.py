6344590587f0d7c0e0b6ec6741f4d4680a60b844
Kevin Orr <kevino@kududyn.com>
2021-02-16 15:11:01 -0500

    Workaround for stack test hanging

---
+++ .ci/scripts/run_stack_test.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+
+import sys
+import os
+import subprocess
+import time
+import re
+
+if os.environ['CI'] != 'true':
+    print('It is not advised to run this script except in a container',
+          file=sys.stderr)
+    exit(1)
+
+job_timeout = int(os.environ.get('STACK_TEST_TIMEOUT', 0))
+start_time = time.time()
+stack = subprocess.Popen(
+    [
+        'stack', 'test', \
+        '--ghc-options', '-fdiagnostics-color=always', \
+        '--test-arguments', '--color', \
+        *sys.argv[1:]
+    ],
+    stdout=subprocess.PIPE,
+    encoding='utf8')
+
+
+def cleanup_and_exit(retcode):
+    stack.terminate()
+    if stack.poll() is None:
+        stack.kill()
+
+    subprocess.run(['pkill', 'stack'])
+    subprocess.run(['pkill', 'binja-test'])
+
+    exit(retcode)
+
+
+if (ret := stack.poll()) is not None:
+    cleanup_and_exit(ret)
+
+for line in stack.stdout:
+    print(line, end='')
+    if re.search(r'examples?, 0 failure', line):
+        cleanup_and_exit(0)
+
+    if re.search(r'examples?, [1-9][0-9]* failure', line):
+        cleanup_and_exit(1)
+
+    if (ret := stack.poll()) is not None:
+        cleanup_and_exit(ret)
+
+print('Bad output from stack?', file=sys.stderr)
+cleanup_and_exit(1)
