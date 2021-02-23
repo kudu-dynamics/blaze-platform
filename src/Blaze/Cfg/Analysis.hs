778b1200067aeddcdf7b053f0c9ccd1798f0c99c
drone <drone@kududyn.com>
2021-02-23 13:23:06 -0700

    WIP: Debugging getDeadBranches.

--- src/Blaze/Cfg/Analysis.hs
+++ src/Blaze/Cfg/Analysis.hs
@@ -1,10 +1,13 @@
 module Blaze.Cfg.Analysis where
 
+import qualified Blaze.Graph as G
 import Blaze.Pil.Analysis (ConstPropState, CopyPropState)
 import qualified Blaze.Pil.Analysis as PA
 import Blaze.Prelude
+import Blaze.Types.Cfg (CfNode (BasicBlock, Call, EnterFunc, LeaveFunc), PilCfg, PilNode)
+import Blaze.Types.Cfg.Interprocedural (InterCfg (InterCfg), unInterCfg)
 import Blaze.Types.Pil (Stmt)
-import Blaze.Types.Cfg (PilNode, CfNode (BasicBlock, Call, EnterFunc, LeaveFunc))
+import qualified Data.Set as Set
 
 -- concat $ G.bfs [(unInterCfg icfg) ^. #root] ((unInterCfg icfg) ^. #graph)
 
@@ -17,19 +20,65 @@ getStmts = \case
 
 -- TODO: May turn this into some sort of fold that updates the
 --       graph's node attributes as we go.
-copyProp :: [[Stmt]] -> [[Stmt]]
-copyProp nodesStmts =
-  PA._copyProp copyPropState <$> nodesStmts
+-- copyProp :: [[Stmt]] -> [[Stmt]]
+-- copyProp nodesStmts =
+--   PA._copyProp copyPropState <$> nodesStmts
+--  where
+--   allStmts :: [Stmt]
+--   allStmts = concat nodesStmts
+--   copyPropState :: CopyPropState
+--   copyPropState = PA.buildCopyPropState allStmts
+copyProp :: InterCfg -> InterCfg
+copyProp icfg =
+  -- PA._copyProp copyPropState <$> nodesStmts
+  InterCfg $ 
+    foldl'
+      ( \cfg_ node ->
+          case G.getNodeAttr node cfg_ of
+            Just stmts ->
+              G.setNodeAttr (PA._copyProp copyPropState stmts) node cfg_
+            Nothing ->
+              cfg_
+      )
+      cfg
+      (Set.toList $ G.nodes cfg)
  where
+  cfg :: PilCfg
+  cfg = unInterCfg icfg
+  nodesStmts :: [[Stmt]]
+  nodesStmts = mapMaybe (`G.getNodeAttr` cfg) $ Set.toList (G.nodes cfg)
   allStmts :: [Stmt]
   allStmts = concat nodesStmts
   copyPropState :: CopyPropState
   copyPropState = PA.buildCopyPropState allStmts
 
-constantProp :: [[Stmt]] -> [[Stmt]]
-constantProp nodesStmts =
-  PA._constantProp constPropState <$> nodesStmts
+-- constantProp :: [[Stmt]] -> [[Stmt]]
+-- constantProp nodesStmts =
+--   PA._constantProp constPropState <$> nodesStmts
+--  where
+--   allStmts :: [Stmt]
+--   allStmts = concat nodesStmts
+--   constPropState :: ConstPropState
+--   constPropState = PA.buildConstPropState allStmts
+constantProp :: InterCfg -> InterCfg
+constantProp icfg =
+  -- PA._constantProp constPropState <$> nodesStmts
+  InterCfg $ 
+    foldl'
+      ( \cfg_ node ->
+          case G.getNodeAttr node cfg_ of
+            Just stmts ->
+              G.setNodeAttr (PA._constantProp constPropState stmts) node cfg_
+            Nothing ->
+              cfg_
+      )
+      cfg
+      (Set.toList $ G.nodes cfg)
  where
+  cfg :: PilCfg
+  cfg = unInterCfg icfg
+  nodesStmts :: [[Stmt]]
+  nodesStmts = mapMaybe (`G.getNodeAttr` cfg) $ Set.toList (G.nodes cfg)
   allStmts :: [Stmt]
   allStmts = concat nodesStmts
   constPropState :: ConstPropState
