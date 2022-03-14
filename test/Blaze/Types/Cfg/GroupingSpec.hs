a8ff63aa597f44e18f4e6e537181cfa07d1f8b48
Kevin Orr <kevino@kududyn.com>
2022-03-14 05:37:26 -0400

    Qualify CFG modules

--- test/Blaze/Types/Cfg/GroupingSpec.hs
+++ test/Blaze/Types/Cfg/GroupingSpec.hs
@@ -3,15 +3,16 @@
 module Blaze.Types.Cfg.GroupingSpec where
 
 import Blaze.Function (Function (Function))
+import Blaze.Types.Cfg
+    ( BasicBlockNode(BasicBlockNode), BranchType(..) )
 import qualified Blaze.Types.Cfg as Cfg
 import qualified Blaze.Graph as G
-import Blaze.Types.Cfg (BranchType(..))
 import Blaze.Prelude
 import Test.Hspec
 import Blaze.Pretty (prettyPrint, pretty, PrettyShow(PrettyShow))
 import Blaze.Types.Pil (Ctx(Ctx), CtxId(CtxId))
 import Blaze.Util.Spec (mkUuid1, mkUuid2)
-import Blaze.Types.Cfg.Grouping
+import qualified Blaze.Types.Cfg.Grouping as Grp
 import qualified Data.HashSet as HashSet
 
 ctx :: Ctx
@@ -19,137 +20,146 @@ ctx = Ctx func . CtxId $ mkUuid1 (0 :: Int)
   where
     func = Function Nothing "foo" 0x00 []
 
-bb :: Ctx -> Address -> Address -> a -> CfNode a
-bb ctx startAddr endAddr x =
-  BasicBlock $ Cfg.BasicBlockNode ctx startAddr endAddr uuid x
- where
-  uuid = mkUuid2 startAddr endAddr
-
-bbn :: Text -> CfNode Text
-bbn name = bb ctx x x name
+gbb :: Address -> Address -> a -> Grp.CfNode a
+cbb :: Address -> Address -> a -> Cfg.CfNode a
+(gbb, cbb) = (go Grp.BasicBlock, go Cfg.BasicBlock)
   where
-    x = fromIntegral $ hash name
+    go f startAddr endAddr x = f $ BasicBlockNode
+      { ctx = Ctx (Function Nothing "f" 0 []) (CtxId $ mkUuid1 (1 :: Int))
+      , start = startAddr
+      , end = endAddr
+      , uuid = uuid'
+      , nodeData = x
+      }
+      where
+        uuid' = mkUuid2 startAddr endAddr
+
+gbbn :: Text -> Grp.CfNode Text
+cbbn :: Text -> Cfg.CfNode Text
+(gbbn, cbbn) = (go gbb, go cbb)
+  where
+    go f n = let x = fromIntegral $ hash n in f x x n
 
 spec :: Spec
 spec = describe "Blaze.Types.Cfg.Grouping" $ do
   context "findNodesInGroup" $ do
     it "should return empty set if group is just start and end with no middle" $ do
-      let cfg = mkCfg
-            (bbn "root")
-            [ bbn "mid"
-            , bbn "end"
+      let cfg = Grp.mkCfg
+            (gbbn "root")
+            [ gbbn "mid"
+            , gbbn "end"
             ]
-            [ CfEdge
-              (bbn "root")
-              (bbn "mid")
+            [ Grp.CfEdge
+              (gbbn "root")
+              (gbbn "mid")
               UnconditionalBranch
-            , CfEdge
-              (bbn "mid")
-              (bbn "end")
+            , Grp.CfEdge
+              (gbbn "mid")
+              (gbbn "end")
               UnconditionalBranch
             ]
-          startNode = bbn "mid"
-          endNode = bbn "end"
+          startNode = gbbn "mid"
+          endNode = gbbn "end"
           expected = HashSet.empty
-      findNodesInGroup startNode endNode cfg `shouldBe` expected
+      Grp.findNodesInGroup startNode endNode cfg `shouldBe` expected
 
     it "should return single middle node" $ do
-      let startNode = bbn "start"
-          midNode = bbn "mid"
-          endNode = bbn "end"
-          cfg = mkCfg
+      let startNode = gbbn "start"
+          midNode = gbbn "mid"
+          endNode = gbbn "end"
+          cfg = Grp.mkCfg
             startNode
             [ midNode
             , endNode
             ]
-            [ CfEdge
+            [ Grp.CfEdge
               startNode
               midNode
               UnconditionalBranch
-            , CfEdge
+            , Grp.CfEdge
               midNode
               endNode
               UnconditionalBranch
             ]
           expected = HashSet.singleton midNode
 
-      findNodesInGroup startNode endNode cfg `shouldBe` expected
+      Grp.findNodesInGroup startNode endNode cfg `shouldBe` expected
 
     it "should return two middle nodes" $ do
-      let startNode = bbn "start"
-          midNode1 = bbn "mid1"
-          midNode2 = bbn "mid2"
-          endNode = bbn "end"
-          cfg = mkCfg
+      let startNode = gbbn "start"
+          midNode1 = gbbn "mid1"
+          midNode2 = gbbn "mid2"
+          endNode = gbbn "end"
+          cfg = Grp.mkCfg
             startNode
             [ midNode1
             , midNode2
             , endNode
             ]
-            [ CfEdge
+            [ Grp.CfEdge
               startNode
               midNode1
               TrueBranch
-            , CfEdge
+            , Grp.CfEdge
               startNode
               midNode2
               FalseBranch
-            , CfEdge
+            , Grp.CfEdge
               midNode2
               endNode
               UnconditionalBranch
-            , CfEdge
+            , Grp.CfEdge
               midNode1
               endNode
               UnconditionalBranch
             ]
           expected = HashSet.fromList [midNode1, midNode2]
 
-      findNodesInGroup startNode endNode cfg `shouldBe` expected
+      Grp.findNodesInGroup startNode endNode cfg `shouldBe` expected
 
-  context "extractGroupingNode" $ do
+  context "Grp.extractGroupingNode" $ do
     it "should get whole cfg in grouping node if they share start and end" $ do
-      let startNode = bbn "start"
-          midNode1 = bbn "mid1"
-          midNode2 = bbn "mid2"
-          endNode = bbn "end"
-          cfg = mkCfg
+      let startNode = gbbn "start"
+          midNode1 = gbbn "mid1"
+          midNode2 = gbbn "mid2"
+          endNode = gbbn "end"
+          cfg = Grp.mkCfg
             startNode
             [ midNode1
             , midNode2
             , endNode
             ]
-            [ CfEdge
+            [ Grp.CfEdge
               startNode
               midNode1
               TrueBranch
-            , CfEdge
+            , Grp.CfEdge
               startNode
               midNode2
               FalseBranch
-            , CfEdge
+            , Grp.CfEdge
               midNode2
               endNode
               UnconditionalBranch
-            , CfEdge
+            , Grp.CfEdge
               midNode1
               endNode
               UnconditionalBranch
             ]
-          expected = GroupingNode endNode (getNodeUUID startNode) cfg
+          expected = Grp.GroupingNode endNode (Grp.getNodeUUID startNode) cfg
 
-          innerGroupNodes = findNodesInGroup startNode endNode cfg
-      extractGroupingNode startNode endNode innerGroupNodes cfg `shouldBe` expected
+          innerGroupNodes = Grp.findNodesInGroup startNode endNode cfg
+      Grp.extractGroupingNode startNode endNode innerGroupNodes cfg `shouldBe` expected
 
     it "should get the inner group but ignore the outer nodes" $ do
-      let rootNode = bbn "root"
-          startNode = bbn "start"
-          midNode1 = bbn "mid1"
-          midNode2 = bbn "mid2"
-          endNode = bbn "end"
-          termNode = bbn "term"
-            
-          outerCfg = mkCfg
+      let rootNode = gbbn "root"
+          startNode = gbbn "start"
+          midNode1 = gbbn "mid1"
+          midNode2 = gbbn "mid2"
+          endNode = gbbn "end"
+          termNode = gbbn "term"
+
+          outerCfg = Grp.mkCfg
             rootNode
             [ startNode
             , midNode1
@@ -157,70 +167,70 @@ spec = describe "Blaze.Types.Cfg.Grouping" $ do
             , endNode
             , termNode
             ]
-            [ CfEdge
+            [ Grp.CfEdge
               rootNode
               startNode
               UnconditionalBranch
-            , CfEdge
+            , Grp.CfEdge
               startNode
               midNode1
               TrueBranch
-            , CfEdge
+            , Grp.CfEdge
               startNode
               midNode2
               FalseBranch
-            , CfEdge
+            , Grp.CfEdge
               midNode2
               endNode
               UnconditionalBranch
-            , CfEdge
+            , Grp.CfEdge
               midNode1
               endNode
               UnconditionalBranch
-            , CfEdge
+            , Grp.CfEdge
               endNode
               termNode
               UnconditionalBranch
             ]
 
-          groupCfg = mkCfg
+          groupCfg = Grp.mkCfg
             startNode
             [ midNode1
             , midNode2
             , endNode
             ]
-            [ CfEdge
+            [ Grp.CfEdge
               startNode
               midNode1
               TrueBranch
-            , CfEdge
+            , Grp.CfEdge
               startNode
               midNode2
               FalseBranch
-            , CfEdge
+            , Grp.CfEdge
               midNode2
               endNode
               UnconditionalBranch
-            , CfEdge
+            , Grp.CfEdge
               midNode1
               endNode
               UnconditionalBranch
             ]
-          expected = GroupingNode endNode (getNodeUUID startNode) groupCfg
+          expected = Grp.GroupingNode endNode (Grp.getNodeUUID startNode) groupCfg
 
-          innerGroupNodes = findNodesInGroup startNode endNode outerCfg
-      extractGroupingNode startNode endNode innerGroupNodes outerCfg `shouldBe` expected
+          innerGroupNodes = Grp.findNodesInGroup startNode endNode outerCfg
+      Grp.extractGroupingNode startNode endNode innerGroupNodes outerCfg `shouldBe` expected
 
   context "makeGrouping" $ do
     it "should replace inner group and connect it to outer nodes" $ do
-      let rootNode = bbn "root"
-          startNode = bbn "start"
-          midNode1 = bbn "mid1"
-          midNode2 = bbn "mid2"
-          endNode = bbn "end"
-          termNode = bbn "term"
-            
-          outerCfg = mkCfg
+      let rootNode = gbbn "root"
+          startNode = gbbn "start"
+          midNode1 = gbbn "mid1"
+          midNode2 = gbbn "mid2"
+          endNode = gbbn "end"
+          termNode = gbbn "term"
+
+          outerCfg = Grp.mkCfg
             rootNode
             [ startNode
             , midNode1
@@ -228,74 +238,73 @@ spec = describe "Blaze.Types.Cfg.Grouping" $ do
             , endNode
             , termNode
             ]
-            [ CfEdge
+            [ Grp.CfEdge
               rootNode
               startNode
               UnconditionalBranch
-            , CfEdge
+            , Grp.CfEdge
               startNode
               midNode1
               TrueBranch
-            , CfEdge
+            , Grp.CfEdge
               startNode
               midNode2
               FalseBranch
-            , CfEdge
+            , Grp.CfEdge
               midNode2
               endNode
               UnconditionalBranch
-            , CfEdge
+            , Grp.CfEdge
               midNode1
               endNode
               UnconditionalBranch
-            , CfEdge
+            , Grp.CfEdge
               endNode
               termNode
               UnconditionalBranch
             ]
 
-          groupCfg = mkCfg
+          groupCfg = Grp.mkCfg
             startNode
             [ midNode1
             , midNode2
             , endNode
             ]
-            [ CfEdge
+            [ Grp.CfEdge
               startNode
               midNode1
               TrueBranch
-            , CfEdge
+            , Grp.CfEdge
               startNode
               midNode2
               FalseBranch
-            , CfEdge
+            , Grp.CfEdge
               midNode2
               endNode
               UnconditionalBranch
-            , CfEdge
+            , Grp.CfEdge
               midNode1
               endNode
               UnconditionalBranch
             ]
-            
-          gnode = Grouping $ GroupingNode endNode (getNodeUUID startNode) groupCfg
 
-          outerCfg' = mkCfg
+          gnode = Grp.Grouping $ Grp.GroupingNode endNode (Grp.getNodeUUID startNode) groupCfg
+
+          outerCfg' = Grp.mkCfg
             rootNode
             [ gnode
             , termNode
             ]
-            [ CfEdge
+            [ Grp.CfEdge
               rootNode
               gnode
               UnconditionalBranch
-            , CfEdge
+            , Grp.CfEdge
               gnode
               termNode
               UnconditionalBranch
             ]
-            
+
           expected = outerCfg'
 
-      PrettyShow (makeGrouping startNode endNode outerCfg) `shouldBe` PrettyShow expected
-
+      PrettyShow (Grp.makeGrouping startNode endNode outerCfg) `shouldBe` PrettyShow expected
