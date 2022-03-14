b12aa694a2ffeeeb077b4ab1dca48a070b10356f
Kevin Orr <kevino@kududyn.com>
2022-03-14 06:14:26 -0400

    Add fromCfg and unfoldGroups tests

--- test/Blaze/Types/Cfg/GroupingSpec.hs
+++ test/Blaze/Types/Cfg/GroupingSpec.hs
@@ -19,6 +19,15 @@ ctx = Ctx func . CtxId $ mkUuid1 (0 :: Int)
   where
     func = Function Nothing "foo" 0x00 []
 
+groupbb :: Grp.CfNode a -> Grp.Cfg a -> Grp.CfNode a
+groupbb end subcfg =
+  Grp.Grouping
+    Grp.GroupingNode
+      { termNode = end
+      , uuid = Grp.getNodeUUID (subcfg ^. #root)
+      , grouping = subcfg
+      }
+
 gbb :: Address -> Address -> a -> Grp.CfNode a
 cbb :: Address -> Address -> a -> Cfg.CfNode a
 (gbb, cbb) = (go Grp.BasicBlock, go Cfg.BasicBlock)
@@ -42,6 +51,98 @@ cbbn :: Text -> Cfg.CfNode Text
 spec :: Spec
 spec = describe "Blaze.Types.Cfg.Grouping" $ do
   let prettyShouldBe x y = PrettyShow x `shouldBe` PrettyShow y
+  let cDoubleDiamond =
+        Cfg.mkCfg
+          (cbbn "root")
+          [ cbbn "mid1"
+          , cbbn "mid2"
+          , cbbn "end"
+          ]
+          [ Cfg.CfEdge (cbbn "root") (cbbn "mid1") TrueBranch
+          , Cfg.CfEdge (cbbn "root") (cbbn "mid2") FalseBranch
+          , Cfg.CfEdge (cbbn "mid1") (cbbn "end") UnconditionalBranch
+          , Cfg.CfEdge (cbbn "mid2") (cbbn "end") UnconditionalBranch
+          ]
+      gDoubleDiamond =
+        Grp.mkCfg
+          (gbbn "root")
+          [ gbbn "mid1"
+          , gbbn "mid2"
+          , gbbn "end"
+          ]
+          [ Grp.CfEdge (gbbn "root") (gbbn "mid1") TrueBranch
+          , Grp.CfEdge (gbbn "root") (gbbn "mid2") FalseBranch
+          , Grp.CfEdge (gbbn "mid1") (gbbn "end") UnconditionalBranch
+          , Grp.CfEdge (gbbn "mid2") (gbbn "end") UnconditionalBranch
+          ]
+      cUngrouped =
+        Cfg.mkCfg
+          (cbbn "root")
+          [ cbbn "branch1"
+          , cbbn "branch11"
+          , cbbn "mid12"
+          , cbbn "mid111"
+          , cbbn "mid112"
+          , cbbn "end11"
+          , cbbn "end1"
+          , cbbn "end"
+          ]
+          [ Cfg.CfEdge (cbbn "root") (cbbn "branch1") UnconditionalBranch
+          , Cfg.CfEdge (cbbn "branch1") (cbbn "branch11") TrueBranch
+          , Cfg.CfEdge (cbbn "branch1") (cbbn "mid12") FalseBranch
+          , Cfg.CfEdge (cbbn "branch11") (cbbn "mid111") TrueBranch
+          , Cfg.CfEdge (cbbn "branch11") (cbbn "mid112") FalseBranch
+          , Cfg.CfEdge (cbbn "mid111") (cbbn "end11") UnconditionalBranch
+          , Cfg.CfEdge (cbbn "mid112") (cbbn "end11") UnconditionalBranch
+          , Cfg.CfEdge (cbbn "end11") (cbbn "end1") UnconditionalBranch
+          , Cfg.CfEdge (cbbn "mid12") (cbbn "end1") UnconditionalBranch
+          , Cfg.CfEdge (cbbn "end1") (cbbn "end") UnconditionalBranch
+          ]
+      gGrouped =
+        let mid111group = groupbb (gbbn "mid111") (Grp.mkCfg (gbbn "mid111") [] [])
+            mid111groupgroup = groupbb mid111group (Grp.mkCfg mid111group [] [])
+            branch11group =
+              groupbb (gbbn "end11") $
+                Grp.mkCfg
+                  (gbbn "branch11")
+                  [ mid111groupgroup
+                  , gbbn "mid112"
+                  , gbbn "end11"
+                  ]
+                  [ Grp.CfEdge (gbbn "branch11") mid111groupgroup TrueBranch
+                  , Grp.CfEdge (gbbn "branch11") (gbbn "mid112") FalseBranch
+                  , Grp.CfEdge mid111groupgroup (gbbn "end11") UnconditionalBranch
+                  , Grp.CfEdge (gbbn "mid112") (gbbn "end11") UnconditionalBranch
+                  ]
+            branch1group =
+              groupbb (gbbn "end1") $
+                Grp.mkCfg
+                  (gbbn "branch1")
+                  [ branch11group
+                  , gbbn "mid12"
+                  , gbbn "end1"
+                  ]
+                  [ Grp.CfEdge (gbbn "branch1") branch11group TrueBranch
+                  , Grp.CfEdge (gbbn "branch1") (gbbn "mid12") FalseBranch
+                  , Grp.CfEdge branch11group (gbbn "end1") UnconditionalBranch
+                  , Grp.CfEdge (gbbn "mid12") (gbbn "end1") UnconditionalBranch
+                  ]
+        in
+          Grp.mkCfg
+            (gbbn "root")
+            [ branch1group
+            , gbbn "end"
+            ]
+            [ Grp.CfEdge (gbbn "root") branch1group UnconditionalBranch
+            , Grp.CfEdge branch1group (gbbn "end") UnconditionalBranch
+            ]
+
+  context "fromCfg" $ do
+    it "should convert a flat CFG to a grouped CFG" $ do
+      Grp.fromCfg cDoubleDiamond `prettyShouldBe` gDoubleDiamond
+  context "unfoldGroups" $ do
+    it "should unfold all groups of a grouped CFG into a flat CFG" $ do
+      Grp.unfoldGroups gGrouped `prettyShouldBe` cUngrouped
   context "findNodesInGroup" $ do
     it "should return empty set if group is just start and end with no middle" $ do
       let cfg = Grp.mkCfg
