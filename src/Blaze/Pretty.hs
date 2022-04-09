4f4a4a6a3df0b92edee5b798f2cbca3b945b528e
mpdairy <mp@kududyn.com>
2022-04-08 23:15:46 -0600

    always show var ctxs

--- src/Blaze/Pretty.hs
+++ src/Blaze/Pretty.hs
@@ -368,12 +368,16 @@ instance Tokenizable Pil.Ctx where
 instance Tokenizable Pil.PilVar where
   tokenize var = do
     ctxIndices' <- view #ctxIndices
-    let ctxIdSuff =
-          either id id $ do
-            if Bimap.size ctxIndices' > 1 then Right () else Left ""
-            ctx <- maybeToEither "@?" (var ^. #ctx)
-            ctxIdIndex <- maybeToEither "@?" $ Bimap.lookupR ctx ctxIndices'
-            pure $ "@" <> show ctxIdIndex
+    let ctxIdSuff = case var ^. #ctx of
+          Nothing -> ""
+          Just ctx -> case Bimap.lookupR ctx ctxIndices' of
+            Nothing -> "@?"
+            Just n -> "@" <> show n
+          -- either id id $ do
+          --   if Bimap.size ctxIndices' > 1 then Right () else Left ""
+          --   ctx <- maybeToEither "@?" (var ^. #ctx)
+          --   ctxIdIndex <- maybeToEither "@?" $ Bimap.lookupR ctx ctxIndices'
+          --   pure $ "@" <> show ctxIdIndex
     pure [varToken $ (var ^. #symbol) <> ctxIdSuff]
 
 tokenizeBinop ::
