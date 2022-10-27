4f80c0d6867d361dab5bf312f4ae59073a38d1b2
mpdairy <mp@kududyn.com>
2022-10-26 19:59:11 -0600

    getMaxAddress

--- src/Ghidra/BasicBlock.hs
+++ src/Ghidra/BasicBlock.hs
@@ -94,3 +94,5 @@ getBasicBlockGraph gs func = do
   edges <- concatMapM (getEdgesForBlock gs $ Set.fromList blocks) blocks
   return $ BasicBlockGraph blocks edges
 
+getMaxAddress :: J.CodeBlock -> IO J.Address
+getMaxAddress bb = Java.call (coerce bb :: J.AddressSetView) "getMaxAddress"
