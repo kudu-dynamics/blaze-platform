701726c2dfa3711ff6a412e015befb56821273b2
mpdairy <mp@kududyn.com>
2023-09-26 21:09:54 -0600

    crippled matcher

--- src/Flint/Analysis/Path/Matcher.hs
+++ src/Flint/Analysis/Path/Matcher.hs
@@ -5,6 +5,7 @@ import Flint.Prelude hiding (Symbol)
 import qualified Blaze.Types.Pil as Pil
 import Blaze.Types.Pil (Expression, Stmt, PilVar, Size)
 
+import qualified Data.HashMap.Strict as HashMap
 
 type Symbol = Text
 
@@ -21,7 +22,7 @@ data Statement expr
   | Constraint expr
   | Store expr expr
   | EnterFunc (Func expr) -- need one that lets you access args of expanded call?
-  | Call (CallDest expr) [expr]
+  | Call (Maybe expr) (CallDest expr) [expr]
   | BranchCond expr
   | Jump expr
   | Ret expr
@@ -31,11 +32,13 @@ data Statement expr
 
 data StmtPattern
   = Stmt (Statement ExprPattern)
-  | Never StmtPattern
-  | AnyOne [StmtPattern]  -- One match succeeds. [] immediately succeeeds
-  | Unordered [StmtPattern] -- matches all, but in no particular order
-  | Ordered [StmtPattern] -- matches all. Good for grouping and scoping Where bounds
-  | Where BoundExpr -- Add a boolean expr constraint, using bound variables
+  -- | Eventually StmtPattern
+  | Avoid StmtPattern
+  -- | AvoidUntil Avoid
+  | AnyOne [StmtPattern]  -- One match succeeds. [] immediately succeeeds.
+  | Unordered [StmtPattern] -- matches all, but in no particular order.
+  | Ordered [StmtPattern] -- matches all. Good for grouping and scoping Where bounds.
+  | Assert BoundExpr -- Add a boolean expr constraint, using bound variables.
   deriving (Eq, Ord, Show, Generic)
 
 data BoundExprSize
@@ -74,13 +77,13 @@ exampleCommBufferOobUsage =
       [ Stmt $ BranchCond . Expr . Pil.CMP_ULT $ Pil.CmpUltOp (Bind "y" Wild) (Bind "max_length" Wild)
       , Stmt $ BranchCond . Expr . Pil.CMP_UGE $ Pil.CmpUgeOp (Bind "max_length" Wild) (Bind "y" Wild)
       ]
-    , Stmt $ Call (CallFunc (FuncName "SmmIsBufferOutsideSmmValid"))
+    , Stmt $ Call (Just Wild) (CallFunc (FuncName "SmmIsBufferOutsideSmmValid"))
       [ Var "arg3"
       , Bind "y" Wild
       ]
     ]
   , Stmt $ Store (Expr . Pil.ADD $ Pil.AddOp (Var "arg3") (Bind "n" Wild)) (Bind "stored_val" Wild)
-  , Where . BoundExpr (ConstSize 8) . Pil.CMP_UGT $ Pil.CmpUgtOp (Bound "n") (Bound "max_length")
+  , Assert . BoundExpr (ConstSize 8) . Pil.CMP_UGT $ Pil.CmpUgtOp (Bound "n") (Bound "max_length")
   ]
                             
 
@@ -137,3 +140,135 @@ exampleCommBufferOobUsage =
 -- jackson = do
 --   stmt $ def (bind "y") (load (var "arg4"))
 
+-- data Avoid = Avoid
+--   { avoid :: StmtPattern
+--   , until :: StmtPattern
+--   } deriving (Eq, Ord, Show, Generic)
+
+data MatcherState = MatcherState
+  { remainingStmts :: [Pil.Stmt]
+  , boundSyms :: HashMap Symbol Pil.Expression
+  , avoids :: [StmtPattern]
+  -- The successfully parsed stmts, stored in reverse order
+  -- possibly interleaved with Assertions
+  , parsedStmtsWithAssertions :: [Either BoundExpr Pil.Stmt]
+  } deriving Generic
+
+newtype Matcher a = Matcher {
+  _runMatcher :: ExceptT () (StateT MatcherState Identity) a
+  }
+  deriving newtype
+    ( Functor
+    , Applicative
+    , Monad
+    , MonadError ()
+    , MonadState MatcherState
+    , Alternative
+    )
+
+runMatcher_ :: Matcher a -> MatcherState ->  (Either () a, MatcherState)
+runMatcher_ action s
+  = runIdentity
+  . flip runStateT s
+  . runExceptT
+  . _runMatcher
+  $ action
+
+mkMatcherState :: [Pil.Stmt] -> MatcherState
+mkMatcherState stmts = MatcherState stmts HashMap.empty [] []
+
+runMatcher :: [Stmt] -> Matcher a -> Maybe (a, MatcherState)
+runMatcher stmts action = case runMatcher_ action (mkMatcherState stmts) of
+  (Left _, _) -> Nothing
+  (Right x, s) -> Just (x, s)
+
+-- matchStmt :: Statement ExprPattern -> Matcher ()
+-- matchStmt stmt = do
+  
+--   \case
+--   Def a b -> 
+
+-- matchStmtPattern :: StmtPattern -> Matcher ()
+-- matchStmtPattern = \case
+--   Stmt s -> matchStmt
+--   _ -> undefined
+
+-- addBoundExpr :: BoundExpr -> Matcher ()
+-- addBoundExpr x = modify (\s -> s & #extraConstraints %~ (x:))
+
+-- | Gets the next statement from remainingStatements
+takeNextStmt :: Matcher (Maybe Pil.Stmt)
+takeNextStmt = use #remainingStmts >>= \case
+  [] -> return Nothing
+  (x:xs) -> do
+    #remainingStmts .= xs
+    return $ Just x
+
+-- | Restores a statement to the beginning of remainingStatements
+restoreStmt :: Pil.Stmt -> Matcher ()
+restoreStmt stmt = #remainingStmts %= (stmt:)
+
+matchStmt :: Pil.Stmt -> Statement ExprPattern -> Matcher ()
+matchStmt stmt sPat = case (stmt, sPat) of
+  _ -> undefined
+
+-- matchAvoid :: StmtPattern -> StmtPattern -> Matcher ()
+-- matchAvoid avoid term = takeStmt >>= \case
+--   Nothing -> return ()
+--   Just stmt -> matchStmtPattern avoid
+
+backtrackOnError :: Matcher a -> Matcher a
+backtrackOnError action = do
+  s <- get
+  action <|> (put s >> throwError ())
+
+addBoundExpr :: BoundExpr -> Matcher ()
+addBoundExpr x = #parsedStmtsWithAssertions %= (Left x :)
+
+storeAsParsed :: Pil.Stmt -> Matcher ()
+storeAsParsed x = #parsedStmtsWithAssertions %= (Right x :)
+
+-- | Matches the next statement with the next stmt pattern.
+matchNextStmt :: StmtPattern -> Matcher ()
+matchNextStmt pat = takeNextStmt >>= \case
+  Nothing -> case pat of
+    Stmt _ -> bad
+    Avoid _ -> good
+    AnyOne [] -> good
+    AnyOne _ -> bad
+    Unordered [] -> good
+    Unordered _ -> bad
+    Ordered [] -> good
+    Ordered _ -> bad
+    Assert boundExpr -> addBoundExpr boundExpr >> good
+    where
+      bad = throwError ()
+      good = return ()
+  Just stmt -> case pat of
+    Stmt sPat -> tryError (matchStmt stmt sPat) >>= \case
+      -- Matched Statement
+      Right _ -> do
+        storeAsParsed stmt
+        return ()
+      -- Stmt failed to match
+      Left _ -> do
+        checkAvoids
+        storeAsParsed stmt
+        matchNextStmt pat
+    Avoid avoid -> do
+      -- An Avoid pattern doesn't consume a statement
+      restoreStmt stmt
+      #avoids %= (avoid:)
+      return ()
+    AnyOne pats -> undefined
+    where
+      -- | Throws error if an avoid has been found.
+      checkAvoids = do
+        restoreStmt stmt
+        avds <- use #avoids
+        tryError (asum $ backtrackOnError . matchNextStmt <$> avds) >>= \case
+          -- Found an avoid
+          Right _ -> throwError ()
+          -- No avoid found. Remove restored stmt
+          Left _ -> takeNextStmt >> return ()
+  
--- src/Flint/Prelude.hs
+++ src/Flint/Prelude.hs
@@ -1,6 +1,7 @@
 module Flint.Prelude
   ( module Exports
   , hoistMaybeM
+  , tryError
   ) where
 
 import Blaze.Prelude as Exports
@@ -13,4 +14,81 @@ import Control.Concurrent.Async as Exports (replicateConcurrently)
 hoistMaybeM :: Monad m => m (Maybe a) -> MaybeT m a
 hoistMaybeM m = lift m >>= maybe mzero return
 
-      
+-- | 'MonadError' analogue to the 'Control.Exception.try' function.
+tryError :: MonadError e m => m a -> m (Either e a)
+tryError action = (Right <$> action) `catchError` (pure . Left)
+
+-- newtype MatcherMonad a = MatcherMonad {
+--   _runMatcher :: StateT Int (ExceptT () Identity) a
+--   }
+--   deriving newtype
+--     ( Functor
+--     , Applicative
+--     , Monad
+--     , MonadError ()
+--     , MonadState Int
+--     , Alternative
+--     )
+
+-- runMatcher :: MatcherMonad a -> Int -> Either () (a, Int)
+-- runMatcher action s
+--   = runIdentity
+--   . runExceptT
+--   . flip runStateT s
+--   . _runMatcher
+--   $ action
+
+-- add5 :: MatcherMonad ()
+-- add5 = modify (+5)
+
+-- add1 :: MatcherMonad ()
+-- add1 = modify (+1)
+
+-- foodoo :: MatcherMonad ()
+-- foodoo = asum [(add5 >> throwError ()), add1, add5]
+
+
+
+
+
+
+
+-- newtype MatcherMonad a = MatcherMonad {
+--   _runMatcher :: ExceptT () (StateT Int Identity) a
+--   }
+--   deriving newtype
+--     ( Functor
+--     , Applicative
+--     , Monad
+--     , MonadError ()
+--     , MonadState Int
+--     , Alternative
+--     )
+
+-- runMatcher :: MatcherMonad a -> Int -> (Either () a, Int)
+-- runMatcher action s
+--   = runIdentity
+--   . flip runStateT s
+--   . runExceptT
+--   . _runMatcher
+--   $ action
+
+-- add5 :: MatcherMonad ()
+-- add5 = modify (+5)
+
+-- add1 :: MatcherMonad ()
+-- add1 = modify (+1)
+
+-- foodoo :: MatcherMonad ()
+-- foodoo = asum $ backtrackOnError <$> [(add5 >> throwError ()), add1, add5]
+
+-- backtrackOnError :: MatcherMonad a -> MatcherMonad a
+-- backtrackOnError action = do
+--   s <- get
+--   action <|> (put s >> throwError ())
+
+-- ifElse :: MonadError e m => m a -> (e -> m b) -> (a -> m b) -> m b
+-- ifElse action ehandler ghandler = do
+--   catchError (Right <$> action) (return . Left) >>= \case
+--     Left e -> ehandler e
+--     Right x -> ghandler x
