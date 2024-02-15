afcec2b072b862c3df1a55a6e03da795f4268f90
Kevin Orr <kevino@kududyn.com>
2024-02-15 05:09:51 +0000

    Improve docker build time and image size

--- ghidra-haskell/scripts/getGhidraJar.sh
+++ ghidra-haskell/scripts/getGhidraJar.sh
@@ -1,14 +1,31 @@
 #!/usr/bin/env bash
 
+case "$#" in
+    0)
+        output=res/ghidra.jar
+        ;;
+    1)
+        output="$1"
+        ;;
+    *)
+        echo "USAGE: $0 [OUTPUT_PATH]" >&2
+        exit 2
+esac
+
 set -euxo pipefail
 
 GHIDRA_RELEASE_ZIP="https://github.com/NationalSecurityAgency/ghidra/releases/download/Ghidra_10.3_build/ghidra_10.3_PUBLIC_20230510.zip"
 GHIDRA_SHA256="4e990af9b22be562769bb6ce5d4d609fbb45455a7a2f756167b8cdcdb75887fc"
 
-curl -fSL --proto '=https' --tlsv1.2 "${GHIDRA_RELEASE_ZIP}" >/tmp/ghidra.zip
-echo "${GHIDRA_SHA256} */tmp/ghidra.zip" | shasum -c -a 256
-mkdir -p /tmp/ghidra_zip
-unzip /tmp/ghidra.zip -d /tmp/ghidra_zip
-cd res && /tmp/ghidra_zip/ghidra*/support/buildGhidraJar
-rm /tmp/ghidra.zip
-rm -r /tmp/ghidra_zip
+d="$(mktemp -d)"
+mkdir -p "$d"/extracted
+function cleanup() {
+    rm -r "$d"
+}
+trap cleanup EXIT
+
+curl -fSL --proto '=https' --tlsv1.2 "${GHIDRA_RELEASE_ZIP}" >"$d"/ghidra.zip
+echo "${GHIDRA_SHA256} /$d/ghidra.zip" | sha256sum -c
+unzip "$d"/ghidra.zip -d "$d"/extracted
+cd "$d"/extracted/ghidra*
+./support/buildGhidraJar -output "$output"
